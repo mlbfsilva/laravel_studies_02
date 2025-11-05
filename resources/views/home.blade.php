ba4c308d6c78c0f93dada9d7cb4f2a80ad3703b2
Maria Luiza Baborsa <marialuiza.bf.silva@gmail.com>
2025-11-05 12:14:52 -0300

    aula - da rota, pelo controller até a view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main_layout')
+@section('content')
+    
+    <p class="display-1 text-center">Hello World</p> 
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
-    echo "Olá mundo!";
-});
+Route::get('/', [MainController::class, 'showView']);
